bd39d818b8e7b9555ca8f66736e9696dd51b2fc5
IsaacKyh <isaac.leanderz@student.kyh.se>
2021-11-04 13:38:23 +0100

    Comments

--- Fil&Classhantering/src/Add.java
+++ Fil&Classhantering/src/Add.java
@@ -24,3 +24,8 @@ public class Add  implements Serializable {
                 ;
     }
 }
+
+
+
+// I used HollowRec before I saw that Add also just have 2 ints if I take away the sums from that class, so I took away sums fom Add
+// and made sums a local variable instead. So I could use Add on both HollowRec and Add
--- Fil&Classhantering/src/HollowRec.java
+++ Fil&Classhantering/src/HollowRec.java
@@ -1,18 +1,21 @@
-public class HollowRec {
+//public class HollowRec {
+//
+//    private final int n;
+//    private final int m;
+//
+//    public HollowRec(int inN, int inM) {
+//        n = inN;
+//        m = inM;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return n + "+" + m;
+//    }
+//}
+//
+//
+//
 
-    private final int n;
-    private final int m;
-
-    public HollowRec(int inN, int inM) {
-        n = inN;
-        m = inM;
-    }
-
-    @Override
-    public String toString() {
-        return n + "+" + m;
-    }
-}
-
-
-
+// I used HollowRec before I saw that Add also just have 2 ints if I take away the sums from that class, so I took away sums fom Add
+// and made sums a local variable instead. So I could use Add on both HollowRec and Add
